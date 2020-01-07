86e2259e68702ccbafd622b4022c97dd7cbe8b08
Robert Alexander <gogonegro@gmail.com>
2020-01-07 17:24:27 +0100

    minor fixes

--- adsl-plot.py
+++ adsl-plot.py
@@ -1,4 +1,4 @@
-"""Plots the adsl-log log."""
+"""Plots the adsl-log generated log."""
 import matplotlib.pyplot as plt
 # import matplotlib.dates as mdates
 import pandas as pd
@@ -6,15 +6,14 @@ import pandas as pd
 # register_matplotlib_converters()
 
 # set field delimiter and set column names which will also cause reading from row 1
-data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'])
-data.head()
-dl_df = data[['datetime', 'down']]
-ul_df = data[['datetime', 'up']]
+data = pd.read_csv("test.log", sep=';', names=[
+                   'datetime', 'severity', 'down', 'up', 'loss', 'server'])
 speeds_df = data[['datetime', 'down', 'up']]
-dl_df.info()
+# speeds_df.info()
 
 y1 = speeds_df.down.plot(grid=True, label="DL", legend=True, linewidth=5)
 y2 = speeds_df.up.plot(secondary_y=True, label="UL", legend=True, linewidth=5)
 y1.set_ylim(180, 215)
 y2.set_ylim(20, 40)
+#  need to fix x axis labels
 plt.show()
