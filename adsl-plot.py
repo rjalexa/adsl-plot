67569da07e35feb02291e490690c795548b2ba13
Robert Alexander <gogonegro@gmail.com>
2020-01-07 17:55:21 +0100

    commented with couple of point to develop further

--- adsl-plot.py
+++ adsl-plot.py
@@ -2,12 +2,13 @@
 import matplotlib.pyplot as plt
 # import matplotlib.dates as mdates
 import pandas as pd
-# from pandas.plotting import register_matplotlib_converters
-# register_matplotlib_converters()
 
 # set field delimiter and set column names which will also cause reading from row 1
 data = pd.read_csv("test.log", sep=';', names=[
                    'datetime', 'severity', 'down', 'up', 'loss', 'server'])
+
+#  we need to filter out ERROR records (with 0 speeds)
+
 speeds_df = data[['datetime', 'down', 'up']]
 # speeds_df.info()
 
