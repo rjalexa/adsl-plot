bdbec773d607a9656f8239a485b397b39b5069c5
Robert Alexander <gogonegro@gmail.com>
2020-01-08 14:53:06 +0100

    need to tame two figures into 1

--- adsl-plot.py
+++ adsl-plot.py
@@ -1,31 +1,31 @@
 """Plots the adsl-log generated log."""
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
+###### Now prepare the data to plot
 # set field delimiter and set column names which will also cause reading including row 1; use the datetime as the index
-data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'], index_col=0)
-data.head()
+# data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'], index_col=0)
+data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'])
+# data.head()
+# data.info()
 # convert the dateime string read by read_csv to datetime64
 data['datetime'] = pd.to_datetime(data['datetime'])
-data.head()
+# data.info()
 # we will need to discard all ERROR lines (DL:0;UL:0)
 indexNames = data[data['severity'] == 'ERROR'].index
 data.drop(indexNames, inplace=True)
 
-#  y1 = dl_df.plot(grid=True, label="DL", legend=True, linewidth=5)
-#  y2 = ul_df.plot(secondary_y=True, label="UL", legend=True, linewidth=5)
-#  y1.set_ylim(180, 215)
-#  y2.set_ylim(20, 40)
+######
+###### Now that data is ready get on with displaying it
+######
 
-
+y1 = data.plot(x='datetime',y='down',grid=True, label="DL", legend=True, linewidth=3)
+y2 = data.plot(x='datetime',y='up', secondary_y=True, label="UL", legend=True, linewidth=3)
+y1.set_ylim(180, 215)
+y2.set_ylim(20, 40)
 y1.set_title('Speeds as monitored by adsl-log')
 y1.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
 # rotate and align the tick labels so they look better
 # fig.autofmt_xdate()
-
-# y1 = ax1.plot(dl_df['datetime'], dl_df['down'], label="DL", linewidth=3)
-# y2 = ax1.plot(ul_df['datetime'], ul_df['up'],   label="UL", linewidth=3)
-#
 
 plt.show()
