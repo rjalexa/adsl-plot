c603d909bd39ebdc64c96a5ab832beb14500da4f
Robert Alexander <gogonegro@gmail.com>
2020-01-10 14:00:52 +0100

    Initial good release

--- adsl-plot.py
+++ adsl-plot.py
@@ -2,30 +2,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-###### Now prepare the data to plot
-# set field delimiter and set column names which will also cause reading including row 1; use the datetime as the index
-# data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'], index_col=0)
-data = pd.read_csv("test.log", sep=';', names=['datetime', 'severity', 'down', 'up', 'loss', 'server'])
-# data.head()
-# data.info()
-# convert the dateime string read by read_csv to datetime64
-data['datetime'] = pd.to_datetime(data['datetime'])
-# data.info()
-# we will need to discard all ERROR lines (DL:0;UL:0)
+###### Prepare the data to plot
+# set field delimiter and set column names which will also cause reading from row 1
+data = pd.read_csv('test.log', sep=';', names=[
+                   'datetime', 'severity', 'down', 'up', 'loss', 'server'])
+#  we need to filter out ERROR records (with 0 speeds)
 indexNames = data[data['severity'] == 'ERROR'].index
 data.drop(indexNames, inplace=True)
+# convert datetime pandas object to datetime64
+data['datetime'] = pd.to_datetime(data['datetime'])
+# use a dataframe with just the data I need; cleaner
+speeds_df = data[['datetime', 'down', 'up']]
 
-######
-###### Now that data is ready get on with displaying it
-######
+# now plot the graph
+fig, ax = plt.subplots()
 
-y1 = data.plot(x='datetime',y='down',grid=True, label="DL", legend=True, linewidth=3)
-y2 = data.plot(x='datetime',y='up', secondary_y=True, label="UL", legend=True, linewidth=3)
-y1.set_ylim(180, 215)
-y2.set_ylim(20, 40)
-y1.set_title('Speeds as monitored by adsl-log')
-y1.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
-# rotate and align the tick labels so they look better
-# fig.autofmt_xdate()
+color = 'tab:red'
+speeds_df.plot(ax=ax, x='datetime', y='down', label="DL", linewidth=2, color=color)
+ax.set_ylabel('DL', color=color)
+ax.tick_params(axis='y', labelcolor=color)
+
+color = 'tab:blue'
+ax2 = speeds_df.plot(ax=ax, x='datetime', y='up', secondary_y=True, label="UL", linewidth=2, color=color)
+ax2.set_ylabel('UL', color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+# cannot show a grid since the two scales are different
+ax.set_ylim(10, 225)
+ax2.set_ylim(15, 50)
+
+color = 'tab:green'
+ax.set_xlabel('time', color=color)
+ax.tick_params(axis='x', labelcolor=color)
 
 plt.show()
